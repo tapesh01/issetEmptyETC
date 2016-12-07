b079fe02970486d51bb1047ac24dc5d3a5d1a96d
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-12-07 16:15:52 -0500

    first commit

---
+++ main.php
@@ -0,0 +1,23 @@
+
+<?php>
+$x = 10;
+$y = 20;
+$z = 10;
+
+if($x < $y){
+	echo "x is bigger than y";
+}
+
+if ($x != $y){
+	echo "x is not equal to y";
+}
+
+if ($x = $z){
+	echo "x is equal to y";
+}
+
+if($x != $z){
+	echo "x is not equal to z";
+}
+
+?>
