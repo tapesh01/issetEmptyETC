de2c2683ddf10306f7ba90ddbbada7696094af64
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-12-10 16:56:28 -0500

    done

--- main.php
+++ main.php
@@ -7,28 +7,33 @@ $i = (2-2);
 
 
 if(isset($x)){
-	print('x is not NULL');
+	print('x is not NULL <br>');
 }
 
 if (empty($y)){
-	print "y is empty";
+	print "y is empty <br>";
 }
 
 if (is_null($z)){
-	print "z is NULL";
+	print "z is NULL <br>";
 }
 
 if(empty($i)){
-	print "i is equal to 0 but considered empty";
+	print "i is equal to 0 but considered empty <br>";
 }
 
 switch($x){
-
 	case NULL:
-	echo "Null value";
+	echo "Null value<br>";
+	break;
+	case 0:
+	echo "value is 0<br>";
 	break;
 }
-
+switch($y){
+	case NULL:
+	echo empty($y);
+}
 
 
 
